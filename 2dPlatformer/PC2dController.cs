2e5120083985d703244fc2f94f8255af9c83029f
Unknown <zanyang1103@163.com>
2018-03-13 23:26:56 +0800

    x输入检测，x运动，无碰撞检测
    检测水平方向输入
    计算水平方向速度
    计算移动位置
    无碰撞检测

--- 2dPlatformer/PC2dController.cs
+++ 2dPlatformer/PC2dController.cs
@@ -4,42 +4,67 @@ using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class PC2dController : MonoBehaviour {
-    private Vector2 _curVelocity;//计算出的当前速度
-    
+
+    public Vector2 _curVelocity = Vector2.zero;//计算出的当前速度
+
+    //重力系数
+    public float gravityModifier = 1;
     //地面运动参数
-    public float groundSpeed;//最大跑动速度
-    public float timeToGroundSpeed;//达到最大速度时间
-    public float groundStopDistance;//停止移动需要的距离
+    public float groundSpeed = 7f;//最大跑动速度
+    public float timeToGroundSpeed = 0.5f;//达到最大速度时间
+    public float groundStopDistance = 0.5f;//停止移动需要的距离
+    public float speedThreshold = 0.1f;//最小移动速度，小于此值速度归零
     private float groundAcceleration;//加速 减速是计算出的
     private float groundDeceleration;//加速 减速是计算出的
 
     private PC2dStateManager stateManager;
-
+    private Rigidbody2D rb2d;
     // Use this for initialization
-    void Start () {
+    void Start() {
         stateManager = new PC2dStateManager();
-        stateManager.OnStateChange += statechanged;
+        stateManager.OnStateChange += Statechanged;
         groundAcceleration = groundSpeed / timeToGroundSpeed;
         groundDeceleration = (groundSpeed * groundSpeed) / (2 * groundStopDistance);
-    }
-    
-    // Update is called once per frame
-    void Update () {
-        //检测输入
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            stateManager.ChangeStateTo(UserState.Rising);
-        }
-        //计算速度
-        //输入与速度同方向加速，反方向减速
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate () {
+    // Update is called once per frame
+    void Update() {
+        //检测输入
+        float xInput = Input.GetAxisRaw("Horizontal");
+
+        //计算速度
+        
+        float inputSign = Mathf.Sign(xInput);//0的sign是1
+        float xVelocitySign = Mathf.Sign(_curVelocity.x);
+        float xVelocity = _curVelocity.x;
+
+        //静止状态，按照输入方向加速
+        if (xVelocity == 0 && xInput != 0) {
+            xVelocity += xInput * groundAcceleration * Time.deltaTime;
+        } else if (xVelocity != 0) {
+            //非静止状态
+            //水平输入与速度同方向加速，反方向减速, 无输入减速
+            if (xInput != 0 && inputSign == xVelocitySign) {
+                xVelocity += xVelocitySign * groundAcceleration * Time.deltaTime;
+            } else {
+                xVelocity += -1 * xVelocitySign * groundDeceleration * Time.deltaTime;
+                xVelocity = xVelocity < speedThreshold ? 0 : xVelocity;
+            }
+        }
+        _curVelocity.x = Mathf.Clamp(xVelocity, -1 * groundSpeed, groundSpeed);
+    }
+
+    void FixedUpdate() {
         //测试碰撞
+        //rb2d.Cast();
         //计算运动
+        Vector2 targetPos = rb2d.position + _curVelocity * Time.deltaTime;
+        rb2d.MovePosition(targetPos);
     }
 
     //监听状态改变
-    void statechanged(UserState fromState, UserState toState) {
+    void Statechanged(UserState fromState, UserState toState) {
         Debug.Log(fromState.ToString() + " => " + toState.ToString());
     }
 }
