75fc78891a9abc4c2ef4b8e70e4dffc46d7d47fb
Unknown <zanyang1103@163.com>
2018-03-13 16:23:26 +0800

    添加地面运动参数，逻辑结构
    添加地面运动需要考虑的参数
    添加一部分地面运动需要的逻辑结构注释

--- 2dPlatformer/PC2dController.cs
+++ 2dPlatformer/PC2dController.cs
@@ -2,21 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class PC2dController : MonoBehaviour {
+    private Vector2 _curVelocity;//计算出的当前速度
+    
+    //地面运动参数
+    public float groundSpeed;//最大跑动速度
+    public float timeToGroundSpeed;//达到最大速度时间
+    public float groundStopDistance;//停止移动需要的距离
+    private float groundAcceleration;//加速 减速是计算出的
+    private float groundDeceleration;//加速 减速是计算出的
 
     private PC2dStateManager stateManager;
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
         stateManager = new PC2dStateManager();
         stateManager.OnStateChange += statechanged;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        groundAcceleration = groundSpeed / timeToGroundSpeed;
+        groundDeceleration = (groundSpeed * groundSpeed) / (2 * groundStopDistance);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        //检测输入
         if (Input.GetKeyDown(KeyCode.Space)) {
             stateManager.ChangeStateTo(UserState.Rising);
         }
-	}
+        //计算速度
+        //输入与速度同方向加速，反方向减速
+    }
+
+    void FixedUpdate () {
+        //测试碰撞
+        //计算运动
+    }
 
     //监听状态改变
     void statechanged(UserState fromState, UserState toState) {
